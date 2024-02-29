145cbbc99f5e25dd866cd9f53ecc70afd97f637a
20019500AWK <20019500@tafe.wa.edu.au>
2024-02-29 14:38:01 +0800

    Made it tell you the price of burger

--- Burger.py
+++ Burger.py
@@ -1,5 +1,7 @@
 print("What is your order?")
 print("We only sell burgers")
 
+burger_cost = "20"
 user_input = input("Your burger here: ") 
-print(user_input)
+if user_input is "Texas Angus" or "Carolina Angus":
+    print(user_input + " burger costs " + burger_cost)
